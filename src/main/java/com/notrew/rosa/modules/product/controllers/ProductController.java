8c7ff8f80a8b5f4707d7961cad990afa7fc19731
Werton <mr.werton@gmail.com>
2023-08-31 11:26:47 -0300

    git c "feat: added documentation"

--- src/main/java/com/notrew/rosa/modules/product/controllers/ProductController.java
+++ src/main/java/com/notrew/rosa/modules/product/controllers/ProductController.java
@@ -7,6 +7,8 @@ import com.notrew.rosa.modules.product.usecases.ChangeStatusProductUseCase;
 import com.notrew.rosa.modules.product.usecases.ListProductsUseCase;
 import com.notrew.rosa.modules.product.usecases.RemoveProductUseCase;
 import com.notrew.rosa.modules.product.usecases.SaveProductUseCase;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/")
+@Tag(name = "rosa")
 public class ProductController {
 
     private final SaveProductUseCase saveProductUseCase;
@@ -37,18 +40,23 @@ public class ProductController {
         return "form";
     }
 
+    @Operation(method = "post", description = "insert new products")
+
     @PostMapping("/create")
     public String processForm(@ModelAttribute(value = "product") ProductDTO product) {
         saveProductUseCase.call(product);
         return "redirect:/";
     }
 
+    @Operation(method = "get", description = "update product status")
     @GetMapping("/update/{id}/{status}")
     public String processForm(@PathVariable("id") String id, @PathVariable("status") String status) {
         final var newData = new ChangeStatusDto(id, status);
         changeStatusProductUseCase.call(newData);
         return "redirect:/";
     }
+
+    @Operation(method = "get", description = "List all the products")
 
     @GetMapping("/")
     public String listProducts(Model model) {
@@ -57,6 +65,7 @@ public class ProductController {
         return "products";
     }
 
+    @Operation(method = "get", description = "delete product")
     @GetMapping("/delete/{id}")
     public String removeProduct(@PathVariable String id, Model model) {
         removeProductUseCase.call(id);
